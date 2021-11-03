492e5b028397aeac4f952fa8d53f8339ddbf26c5
chalkyjeans <aaryan.verma@hotmail.com>
2021-11-03 17:34:11 +0000

    Checks if user has the correct role

--- src/main/java/dev/taboo/taboo/commands/Prefix.kt
+++ src/main/java/dev/taboo/taboo/commands/Prefix.kt
@@ -31,54 +31,71 @@ class Prefix: SlashCommand() {
 
     override fun execute(event: SlashCommandEvent) {
         val user = event.user
+        val member = event.member
         val guild = event.guild
         val id = guild!!.id
         val newPrefix = event.getOption("prefix")!!.asString
         val selfMember = guild.getMemberById(PropertiesManager.botId)
-        transaction {
-            Prefix.replace {
-                it[guildId] = id
-                it[prefix] = newPrefix
+        val manager = guild.getRolesByName("Taboo Manager", true)
+        val isManager = member!!.roles.stream().anyMatch { manager.contains(it) }
+        if (isManager) {
+            transaction {
+                Prefix.replace {
+                    it[guildId] = id
+                    it[prefix] = newPrefix
+                }
             }
-        }
-        event.replyEmbeds(prefixEmbed(user, newPrefix)).mentionRepliedUser(false).setEphemeral(false).queue {
-            selfMember!!.modifyNickname("[$newPrefix] Taboo").queue()
-        }
+            event.replyEmbeds(prefixEmbed(user, newPrefix)).mentionRepliedUser(false).setEphemeral(false).queue {
+                selfMember!!.modifyNickname("[$newPrefix] Taboo").queue()
+            }
+        } else event.replyEmbeds(noRoleEmbed(user)).mentionRepliedUser(false).setEphemeral(true).queue()
     }
 
     override fun execute(event: CommandEvent) {
         val user = event.author
+        val member = event.member
         val guild = event.guild
         val id = guild!!.id
         val newPrefix = event.args
         val message = event.message
         val selfMember = guild.getMemberById(PropertiesManager.botId)
-        if (newPrefix.isEmpty()) {
-            message.replyEmbeds(noArgsEmbed(user)).mentionRepliedUser(false).queue()
-            return
-        }
-        transaction {
-            Prefix.replace {
-                it[guildId] = id
-                it[prefix] = newPrefix
+        val manager = guild.getRolesByName("Taboo Manager", true)
+        val isManager = member!!.roles.stream().anyMatch { manager.contains(it) }
+        if (isManager) {
+            if (newPrefix.isEmpty()) {
+                message.replyEmbeds(noArgsEmbed(user)).mentionRepliedUser(false).queue()
+                return
             }
-        }
-        message.replyEmbeds(prefixEmbed(user, newPrefix)).mentionRepliedUser(false).queue {
-            selfMember!!.modifyNickname("[$newPrefix] Taboo").queue()
-        }
+            transaction {
+                Prefix.replace {
+                    it[guildId] = id
+                    it[prefix] = newPrefix
+                }
+            }
+            message.replyEmbeds(prefixEmbed(user, newPrefix)).mentionRepliedUser(false).queue {
+                selfMember!!.modifyNickname("[$newPrefix] Taboo").queue()
+            }
+        } else message.replyEmbeds(noRoleEmbed(user)).mentionRepliedUser(false).queue()
     }
 
     private fun prefixEmbed(user: User, newPrefix: String): MessageEmbed {
         return ResponseHelper.generateSuccessEmbed(
             user, "Prefix changed to `$newPrefix`",
-            null, Color.GREEN
+            "", Color.GREEN
         )
     }
 
     private fun noArgsEmbed(user: User): MessageEmbed {
         return ResponseHelper.generateFailureEmbed(
             user, "No arguments were provided!",
-            null
+            ""
+        )
+    }
+
+    private fun noRoleEmbed(user: User): MessageEmbed {
+        return ResponseHelper.generateFailureEmbed(
+            user, "You do not have the required role to use this command!",
+            ""
         )
     }
 
