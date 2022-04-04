507ea5478fc03744546222ae22b665f8d4b100d5
chalkyjeans <aaryan.verma@hotmail.com>
2022-04-04 19:44:48 +0100

    Update to Kotlin 1.6.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    kotlin("jvm") version "1.6.20-RC"
+    kotlin("jvm") version "1.6.20"
 }
 
 group = "xyz.chalky.taboo"
