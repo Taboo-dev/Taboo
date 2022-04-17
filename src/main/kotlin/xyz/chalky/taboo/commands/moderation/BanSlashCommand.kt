8e9f06e8faab90cab246823c0ffdab48ec64860b
chalkyjeans <aaryan.verma@hotmail.com>
2022-04-17 16:38:37 +0100

    Catch exceptions

--- src/main/kotlin/xyz/chalky/taboo/commands/moderation/BanSlashCommand.kt
+++ src/main/kotlin/xyz/chalky/taboo/commands/moderation/BanSlashCommand.kt
@@ -4,6 +4,7 @@ import dev.minn.jda.ktx.Embed
 import dev.minn.jda.ktx.interactions.getOption
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.exceptions.HierarchyException
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import org.jetbrains.exposed.sql.select
@@ -31,53 +32,61 @@ class BanSlashCommand : SlashCommand() {
         val target = event.getOption<User>("user")
         val delDays = if (event.getOption<Int>("del-days") == null) 0 else event.getOption<Int>("del-days")
         val reason = if (event.getOption<String>("reason") == null) "No reason provided." else event.getOption<String>("reason")
-        event.guild!!.ban(target!!, delDays!!, reason).queue({ // Success
-            transaction {
-                val actionLogId = Config.select {
-                    Config.guildId eq event.guild!!.idLong
-                }.firstOrNull()?.getOrNull(Config.actionLog) ?: return@transaction
-                val actionLog = event.guild!!.getTextChannelById(actionLogId)
-                val embed = Embed {
-                    title = "Banned a member"
-                    color = Color.CYAN.hashCode()
-                    author {
-                        name = "Moderator: ${event.user.asTag}"
-                        iconUrl = event.user.effectiveAvatarUrl
+        try {
+            event.guild!!.ban(target!!, delDays!!, reason).queue({ // Success
+                transaction {
+                    val actionLogId = Config.select {
+                        Config.guildId eq event.guild!!.idLong
+                    }.firstOrNull()?.getOrNull(Config.actionLog) ?: return@transaction
+                    val actionLog = event.guild!!.getTextChannelById(actionLogId)
+                    val embed = Embed {
+                        title = "Banned a member"
+                        color = Color.CYAN.hashCode()
+                        author {
+                            name = "Moderator: ${event.user.asTag}"
+                            iconUrl = event.user.effectiveAvatarUrl
+                        }
+                        field {
+                            name = "Banned:"
+                            value = "${target.asMention} ${target.asTag}"
+                            inline = false
+                        }
+                        field {
+                            name = "Days of messages deleted:"
+                            value = "$delDays"
+                            inline = false
+                        }
+                        field {
+                            name = "Reason:"
+                            value = reason!!
+                            inline = false
+                        }
+                        footer {
+                            name = "User ID: ${target.id}"
+                        }
+                        timestamp = Instant.now()
                     }
-                    field {
-                        name = "Banned:"
-                        value = "${target.asMention} ${target.asTag}"
-                        inline = false
-                    }
-                    field {
-                        name = "Days of messages deleted:"
-                        value = "$delDays"
-                        inline = false
-                    }
-                    field {
-                        name = "Reason:"
-                        value = reason!!
-                        inline = false
-                    }
-                    footer {
-                        name = "User ID: ${target.id}"
-                    }
-                    timestamp = Instant.now()
+                    event._reply(embed).setEphemeral(true)
+                        .flatMap { actionLog!!.sendMessageEmbeds(embed) }
+                        .queue()
                 }
-               event._reply(embed).setEphemeral(true).queue {
-                   actionLog!!.sendMessageEmbeds(embed).queue()
-               }
+            }, { // Failure
+                error(event)
+            })
+        } catch (e: HierarchyException) {
+            error(event)
+        }
+    }
+
+    private fun error(event: SlashCommandInteractionEvent) {
+        event._reply(
+            Embed {
+                title = "Failed to ban member"
+                description = "The member could not be banned."
+                color = Color.RED.hashCode()
+                timestamp = Instant.now()
             }
-        }, { // Failure
-            event._reply(
-                Embed {
-                    title = "Failed to ban member"
-                    description = "The member could not be banned."
-                    color = Color.RED.hashCode()
-                    timestamp = Instant.now()
-                }
-            ).setEphemeral(true).queue()
-        })
+        ).setEphemeral(true).queue()
     }
 
 }
--- src/main/kotlin/xyz/chalky/taboo/commands/moderation/KickSlashCommand.kt
+++ src/main/kotlin/xyz/chalky/taboo/commands/moderation/KickSlashCommand.kt
@@ -4,6 +4,7 @@ import dev.minn.jda.ktx.Embed
 import dev.minn.jda.ktx.interactions.getOption
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.exceptions.HierarchyException
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import org.jetbrains.exposed.sql.select
@@ -25,47 +26,55 @@ class KickSlashCommand : SlashCommand() {
     override fun executeCommand(event: SlashCommandInteractionEvent) {
         val target = event.getOption<Member>("user")
         val reason = if (event.getOption<String>("reason") == null) "No reason provided." else event.getOption<String>("reason")
-        target!!.kick(reason).queue ({
-            transaction {
-                val actionLogId = Config.select {
-                    Config.guildId eq event.guild!!.idLong
-                }.firstOrNull()?.getOrNull(Config.actionLog) ?: return@transaction
-                val actionLog = event.guild!!.getTextChannelById(actionLogId)
-                val embed = Embed {
-                    title = "Kicked a member"
-                    author {
-                        name = "Moderator: ${event.user.asTag}"
-                        iconUrl = event.user.effectiveAvatarUrl
+        try {
+            target!!.kick(reason).queue ({
+                transaction {
+                    val actionLogId = Config.select {
+                        Config.guildId eq event.guild!!.idLong
+                    }.firstOrNull()?.getOrNull(Config.actionLog) ?: return@transaction
+                    val actionLog = event.guild!!.getTextChannelById(actionLogId)
+                    val embed = Embed {
+                        title = "Kicked a member"
+                        author {
+                            name = "Moderator: ${event.user.asTag}"
+                            iconUrl = event.user.effectiveAvatarUrl
+                        }
+                        field {
+                            name = "Kicked:"
+                            value = "${target.asMention} ${target.user.asTag}"
+                            inline = false
+                        }
+                        field {
+                            name = "Reason:"
+                            value = reason!!
+                            inline = false
+                        }
+                        footer {
+                            name = "User ID: ${target.id}"
+                        }
+                        timestamp = Instant.now()
                     }
-                    field {
-                        name = "Kicked:"
-                        value = "${target.asMention} ${target.user.asTag}"
-                        inline = false
+                    event._reply(embed).setEphemeral(true).queue {
+                        actionLog!!.sendMessageEmbeds(embed).queue()
                     }
-                    field {
-                        name = "Reason:"
-                        value = reason!!
-                        inline = false
-                    }
-                    footer {
-                        name = "User ID: ${target.id}"
-                    }
-                    timestamp = Instant.now()
                 }
-                event._reply(embed).setEphemeral(true).queue {
-                    actionLog!!.sendMessageEmbeds(embed).queue()
-                }
+            }, {
+                error(event)
+            })
+        } catch (e: HierarchyException) {
+            error(event)
+        }
+    }
+
+    private fun error(event: SlashCommandInteractionEvent) {
+        event._reply(
+            Embed {
+                title = "Failed to kick member"
+                description = "The member could not be kicked."
+                color = Color.RED.hashCode()
+                timestamp = Instant.now()
             }
-        }, {
-            event._reply(
-                Embed {
-                    title = "Failed to kick member"
-                    description = "The member could not be kicked."
-                    color = Color.RED.hashCode()
-                    timestamp = Instant.now()
-                }
-            ).setEphemeral(true).queue()
-        })
+        ).setEphemeral(true).queue()
     }
 
 }
