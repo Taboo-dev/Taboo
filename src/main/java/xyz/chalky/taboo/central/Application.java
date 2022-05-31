033c496877e6f7c5ea94c160aafb0fde9b46dcbc
chalkyjeans <aaryan.verma@hotmail.com>
2022-05-31 11:10:55 +0100

    add shutdown hook

--- src/main/java/xyz/chalky/taboo/central/Application.java
+++ src/main/java/xyz/chalky/taboo/central/Application.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import xyz.chalky.taboo.commands.misc.ShutdownCommand;
 import xyz.chalky.taboo.util.ApplicationContextProvider;
 
 @SpringBootApplication
@@ -34,6 +37,9 @@ public class Application {
                       |____|  (____  /___  /\\____/ \\____/\s
                                    \\/    \\/              \s
                     """);
+        });
+        application.addListeners((ApplicationListener<ContextClosedEvent>) event -> {
+            Taboo.getInstance().getWebhookClient().send(ShutdownCommand.shutdownEmbed);
         });
         application.run(args);
     }
--- src/main/java/xyz/chalky/taboo/commands/misc/ShutdownCommand.java
+++ src/main/java/xyz/chalky/taboo/commands/misc/ShutdownCommand.java
@@ -1,6 +1,5 @@
 package xyz.chalky.taboo.commands.misc;
 
-import club.minnced.discord.webhook.external.JDAWebhookClient;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -17,6 +16,12 @@ import java.util.concurrent.TimeUnit;
 
 public class ShutdownCommand extends Command {
 
+    public static final MessageEmbed shutdownEmbed = new EmbedBuilder()
+            .setTitle("Shutting down Taboo...")
+            .setColor(Color.RED)
+            .setTimestamp(Instant.now())
+            .build();
+
     public ShutdownCommand() {
         super("shutdown", "Shuts down the bot", "shutdown");
         addCommandFlags(CommandFlag.DEVELOPER_ONLY, CommandFlag.PRIVATE);
@@ -26,17 +31,10 @@ public class ShutdownCommand extends Command {
     @Override
     public void executeCommand(@NotNull MessageReceivedEvent event, CommandContext ctx) {
         ScheduledExecutorService executor = Taboo.getInstance().getScheduledExecutor();
-        JDAWebhookClient webhookClient = Taboo.getInstance().getWebhookClient();
-        MessageEmbed embed = new EmbedBuilder()
-                .setTitle("Shutting down Taboo...")
-                .setColor(Color.RED)
-                .setTimestamp(Instant.now())
-                .build();
-        event.getMessage().replyEmbeds(embed).queue();
+        event.getMessage().replyEmbeds(shutdownEmbed).queue();
         Taboo.getLogger().info("Shutting down Taboo...");
         Taboo.getInstance().getShardManager().shutdown();
         Taboo.getLogger().info("Goodbye!");
-        webhookClient.send(embed);
         executor.schedule(() -> {
             System.exit(0);
         }, 10, TimeUnit.SECONDS);
